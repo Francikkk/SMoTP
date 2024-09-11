956f4a07d1adb9bcaeed5e698fea01b40bbb7c1b
Francikkk <101191450+Francikkk@users.noreply.github.com>
2024-09-11 23:54:22 +0200

    Add files via upload

---
+++ SMTP_monday_motivational.py
@@ -0,0 +1,24 @@
+import datetime as dt
+import smtplib
+import random
+
+MY_EMAIL = ""
+MY_PASSWORD = ""
+RECEIVER = ""
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=RECEIVER,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
+
